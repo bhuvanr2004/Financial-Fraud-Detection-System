51f02240172821e763a8f4a324786678e6965887
Bhuvan kumar R <168087438+bhuvanr2004@users.noreply.github.com>
2025-07-04 19:22:38 +0530

    Add files via upload

--- notebooks/repo/urls.py
+++ notebooks/repo/urls.py
@@ -1,9 +1,39 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for fraud_detection_django project.
 
-app_name = 'fraud_app'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('train-model/', views.train_model, name='train_model'),
+    # Admin interface
+    path('admin/', admin.site.urls),
+    
+    # Main application
+    path('', include('fraud_app.urls')),
+    
+    # Authentication
+    # path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# Serve static and media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'fraud_app.views.custom_404'
+handler500 = 'fraud_app.views.custom_500'
